f736970cba5a4fac2a8f13928b8d3e2317a64dbc
soystella <gbunnykleo@gmail.com>
2026-06-07 03:18:01 +0200

    Added fullstack study resource post

--- scripts/generate-timeline.js
+++ scripts/generate-timeline.js
@@ -1,16 +1,54 @@
+//Configuration
+let genTimelineConfig = {
+      mainDir: "./src/content/",
+      mode: "include", //include or exclude
+      only: ['projects', 'posts'],
+      deleteOldTimeline: true
+}
+
+//Project dependencies and basic functions
 import fs from 'fs'
 import yaml from 'js-yaml'
-
 let list = []
+function getConfigFromMd(rawMD) {
+      let config = rawMD.split('---')[1]
+      return yaml.load(config)
+}
+async function getChildren(dir) {
+      return new Promise((resolve, reject) => {
+            fs.readdir(dir, (err, files) => {if (err) {console.error("Error reading timeline directory:", err);reject(err);return;}
+            resolve([files, dir]);
+            });
+      });
+}
+async function generateTimeline(files, dir) {
+      files.forEach(async (file) => {
+            if (file.endsWith('.md')) {
+                  const config = getConfigFromMd(await fs.promises.readFile(dir + file, 'utf8'));
+                  if (!!!config) return;
+                  list.push({"title": config.title,"description": config.description,"type": "project","startDate": config.startDate,"endDate": "","skills": config.techStack,"achievements": ["Task completed"],"links": [{"url": config.liveDemo,"name": "Live Demo","type": "project"}],"icon": "material-symbols:code","featured": config.featured,"color": "#7C3AED",});
+                  console.log(file, ' ', config);
+            } else if (file.endsWith('.json')) {
+                  const config = JSON.parse(await fs.promises.readFile(dir + file, 'utf8'));
+                  if (!!!config) return;
+                  list.push({title: config.title, description: config.description,"type": "project","startDate": config.published, "endDate": "","skills": config.tags, "achievements": ["Task completed"],"links": [{"url": dir.split('content/')[1],"name": "Post","type": "post"}],"icon": "material-symbols:code","color": "#ed903a","featured": config.featured});
+            } else if (!file.includes('.')) {
+                  generateTimeline(...await getChildren(dir + file + '/'));
+            }
+      })
+}
 
-fs.readdir('./src/content/timeline', (err, files) => {
+//Start of Script
+
+//Delete old timeline
+genTimelineConfig.deleteOldTimeline && fs.readdir('./src/content/timeline', (err, files) => {
       if (err) {
             console.error("Error reading timeline directory:", err);
             return;
       }
       // Process the files...
       files.forEach(file => {
-            if(file == "template.json") return;
+            if (file == "first-template-project.json") return;
             fs.rm('./src/content/timeline/' + file, { recursive: true }, (err) => {
                   if (err) {
                         console.error(`Error deleting ${file}:`, err);
@@ -20,93 +58,29 @@ fs.readdir('./src/content/timeline', (err, files) => {
             });
       })
 });
-
-function getConfigFromMd(rawMD) {
-      let config = rawMD.split('---')[1]
-      return yaml.load(config)
-}
-async function getChildren(dir) {
-      return new Promise((resolve, reject) => {
-            fs.readdir(dir, (err, files) => {
-                  if (err) {
-                        console.error("Error reading timeline directory:", err);
-                        reject(err);
-                        return;
-                  }
-                  // Process the files...
-                  resolve([files, dir]);
-            });
+//define mode of timeline generation
+if(genTimelineConfig.mode == "exclude") {
+      getChildren('./src/content/').then(([files, dir]) => {
+            let newFiles = files.filter(file => !genTimelineConfig.only.includes(file));
+            generateTimeline(newFiles, dir);
       });
-}
-async function generateTimeline(files, dir) {
-      files.forEach(async (file) => {
-//______________________________________________________________________________________________________
-            if (file.endsWith('.md')) {
-                  const rawMD = await fs.promises.readFile(dir + file, 'utf8');
-                  const config = getConfigFromMd(rawMD);
-                  if(!!!config) return;
-                  console.log(file, ' ', dir);
-                  const newConfig = {
-                        "title": config.title,
-                        "description": config.description,
-                        "type": "project",
-                        "startDate": config.startDate,
-                        "endDate": "",
-                        "skills": config.techStack,
-                        "achievements": ["Task completed"],
-                        "links": [
-                              {
-                                    "name": "Live Demo",
-                                    "url": config.liveDemo,
-                                    "type": "project"
-                              }
-                        ],
-                        "icon": "material-symbols:code",
-                        "color": "#7C3AED",
-                        "featured": config.featured
-                  }
-                  list.push(newConfig);
-                  console.log(file, ' ', config);
-            } else if (file.endsWith('.json')) {
-//______________________________________________________________________________________________________
-                  const rawJSON = await fs.promises.readFile(dir + file, 'utf8');
-                  const config = JSON.parse(rawJSON);
-                  const newConfig = {
-                        "title": config.title,
-                        "description": config.description,
-                        "type": "project",
-                        "startDate": config.published,
-                        "endDate": "",
-                        "skills": config.tags,
-                        "achievements": ["Task completed"],
-                        "links": [
-                              {
-                                    "name": "Post",
-                                    "url": dir.split('content/')[1],
-                                    "type": "post"
-                              }
-                        ],
-                        "icon": "material-symbols:code",
-                        "color": "#ed903a",
-                        "featured": config.featured
-                  }
-                  list.push(newConfig);
-            } else if (!file.includes('.')) {
-//______________________________________________________________________________________________________
-                  generateTimeline(...await getChildren(dir + file + '/'));
-            }
+} else if(genTimelineConfig.mode == "include") {
+      getChildren('./src/content/').then(([files, dir]) => {
+            let newFiles = files.filter(file => genTimelineConfig.only.includes(file));
+            generateTimeline(newFiles, dir);
       });
+} else {
+      console.error("Invalid mode in genTimelineConfig. Please use 'include' or 'exclude'.");
 }
-getChildren('./src/content/').then(([files, dir]) => {
-      generateTimeline(files, dir);
-});
 setTimeout(() => {
-      list.map((item, index) => {
-            if(!!!item.title) return;
-            fs.writeFile('./src/content/timeline/' + item.title + '.json', JSON.stringify(item, null, 2), (err) => {
-                  if (err) {
-                        console.error(`Error writing ${item.title}.json:`, err);
-                  }
-            });
-      })
-}, 3000);
+      setTimeout(() => {
+            list.map((item, index) => {
+                  if (!!!item.title) return;
+                  fs.writeFile('./src/content/timeline/' + item.title + '.json', JSON.stringify(item, null, 2), (err) => {
+                        if (err) {
+                              console.error(`Error writing ${item.title}.json:`, err);
+                        }
+                  });
+            })
+      }, 50 * list.length);
+}, 500);
